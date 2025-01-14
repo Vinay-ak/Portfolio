bf11de5645f132c9b4da64e0e5202a4dca850e64
Vinayak Shree <vinayakshree2004@gmail.com>
2025-01-14 19:07:04 +0530

    Hero Completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,107 @@
+
+
+// Modules
+import PropTypes from "prop-types"
+
+
+//Primary Button
+const ButtonPrimary = ({
+    lable,
+    href,
+    target = '_self',
+    icon,
+    classes,
+}) => {
+    if (href) {
+        return (
+            <a 
+            href={href}
+            target={target}
+            className={"btn btn-primary " + classes}
+            >
+                {lable}
+
+                {icon ? 
+                <span className="material-symbols-rounded" aria-hidden='true'>{icon}
+                </span> 
+                : undefined
+                }
+            </a>
+        )
+}else {
+    return (
+        <button className={"btn btn-primary " + classes}>
+            {lable}
+
+            {icon ? 
+                <span className="material-symbols-rounded" aria-hidden='true'>{icon}
+                </span> 
+                : undefined
+                }
+        </button>
+    )
+}
+}
+
+ButtonPrimary.propTypes = {
+    lable: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    icon: PropTypes.string,
+    classes: PropTypes.string,
+}
+
+
+//Outline Button
+const ButtonOutline = ({
+    lable,
+    href,
+    target = '_self',
+    icon,
+    classes,
+}) => {
+    if (href) {
+        return (
+            <a 
+            href={href}
+            target={target}
+            className={"btn btn-outline " + classes}
+            >
+                {lable}
+
+                {icon ? 
+                <span className="material-symbols-rounded" aria-hidden='true'>{icon}
+                </span> 
+                : undefined
+                }
+            </a>
+        )
+}else {
+    return (
+        <button className={"btn btn-outline " + classes}>
+            {lable}
+
+            {icon ? 
+                <span className="material-symbols-rounded" aria-hidden='true'>{icon}
+                </span> 
+                : undefined
+                }
+        </button>
+    )
+}
+}
+
+ButtonOutline.propTypes = {
+    lable: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    icon: PropTypes.string,
+    classes: PropTypes.string,
+}
+
+
+
+export {
+    ButtonPrimary,
+    ButtonOutline
+}
