c6c78592e126f371a9faeb63d175d40258a2acb7
Vinayak Shree <vinayakshree2004@gmail.com>
2025-01-14 19:47:32 +0530

    About Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
     <Header />
     <main>
       <Hero />
+      <About />
     </main>
     </>
   );
